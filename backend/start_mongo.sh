ed92943295408aecc14d10b57b346749b6e9e79c
Disklo <rafaellcarvalho5@gmail.com>
2025-12-14 20:59:58 -0300

    Ajustado script de inicialização do banco

--- backend/start_mongo.sh
+++ backend/start_mongo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Cria o diretório de dados se não existir
+if [ ! -d "./data/db" ]; then
+    echo "Creating data/db directory..."
+    mkdir -p ./data/db
+fi
+
 echo "Starting MongoDB..."
 mongod --dbpath ./data/db
